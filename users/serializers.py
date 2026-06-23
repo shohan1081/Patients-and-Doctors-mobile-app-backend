0355d287588d37f0d3b34c8542b7173964badcda
Shohan <shohanulislam19892@gmail.com>
2026-06-23 22:05:34 +0600

    Implement provider registration and auth serializers

---
+++ users/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class ProviderRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    confirm_password = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = User
+        fields = (
+            'full_name', 'email', 'phone_number', 'organization', 
+            'license_number', 'title', 'license_file', 
+            'password', 'confirm_password'
+        )
+        extra_kwargs = {
+            'full_name': {'required': True},
+            'email': {'required': True},
+            'phone_number': {'required': True},
+            'organization': {'required': True},
+            'license_number': {'required': True},
+            'title': {'required': True},
+            'license_file': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        password = validated_data.pop('password')
+        user = User.objects.create(
+            role=User.PROVIDER,
+            is_active=True, # User can be active but not verified
+            is_verified=False, # This is our custom verification flag
+            **validated_data
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'full_name', 'role', 'is_verified')
