e6e5572e2c924d0929cb92c0d4100d8ce4a4ee0f
Shohan <shohanulislam19892@gmail.com>
2026-06-24 16:54:48 +0600

    Set up ASGI routing ProtocolTypeRouter for HTTP and WebSockets connections

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,25 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import models.
+django_asgi_app = get_asgi_application()
+
+# Import routing details after initializing get_asgi_application
+import chat.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
