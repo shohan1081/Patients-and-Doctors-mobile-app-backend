f66a87f9623b29444300a3c2d61ac8c0e11b6975
Shohan <shohanulislam19892@gmail.com>
2026-06-27 17:44:39 +0600

    Register chat and attachments upload views URL routing

--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ChatRoomViewSet, RoomMessagesListView
+from .views import ChatRoomViewSet, RoomMessagesListView, ChatAttachmentUploadView
 
 router = DefaultRouter()
 router.register('rooms', ChatRoomViewSet, basename='chat_room')
 
 urlpatterns = [
     path('rooms/<int:room_id>/messages/', RoomMessagesListView.as_view(), name='room_messages'),
+    path('messages/upload/', ChatAttachmentUploadView.as_view(), name='chat_message_upload'),
     path('', include(router.urls)),
 ]
