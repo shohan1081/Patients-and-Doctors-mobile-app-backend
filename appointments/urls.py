8b5e23169aaebbf0826b963096a6adeb72be89b2
Shohan <shohanulislam19892@gmail.com>
2026-06-27 17:44:38 +0600

    Register router urls for appointments and protocols

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DoctorAvailabilityViewSet, AppointmentViewSet, GetAvailableSlotsView
+from .views import (
+    DoctorAvailabilityViewSet, AppointmentViewSet, GetAvailableSlotsView, 
+    DoctorPatientRelationViewSet, ProtocolViewSet
+)
 
 router = DefaultRouter()
 router.register('availability', DoctorAvailabilityViewSet, basename='doctor_availability')
 router.register('bookings', AppointmentViewSet, basename='appointment')
+router.register('patients', DoctorPatientRelationViewSet, basename='doctor_patient_relation')
+router.register('protocols', ProtocolViewSet, basename='protocol')
 
 urlpatterns = [
     path('available-slots/', GetAvailableSlotsView.as_view(), name='available_slots'),
