a893306a411009adc8b89d763a019c6f688532bf
Shohan <shohanulislam19892@gmail.com>
2026-06-27 17:44:40 +0600

    Add search, profiles, and validation URL mappings

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import ProviderRegistrationView, CustomTokenObtainPairView, ProviderProfileView
+from .views import (
+    ProviderRegistrationView, PatientRegistrationView, CustomTokenObtainPairView, 
+    ProviderProfileView, PatientProfileView, ChangePasswordView, VerifiedDoctorListView,
+    PatientListView
+)
 
 urlpatterns = [
     path('register/provider/', ProviderRegistrationView.as_view(), name='register_provider'),
+    path('register/patient/', PatientRegistrationView.as_view(), name='register_patient'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProviderProfileView.as_view(), name='provider_profile'),
+    path('profile/patient/', PatientProfileView.as_view(), name='patient_profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
+    path('doctors/', VerifiedDoctorListView.as_view(), name='verified_doctors'),
+    path('patients/', PatientListView.as_view(), name='patient_list'),
 ]
