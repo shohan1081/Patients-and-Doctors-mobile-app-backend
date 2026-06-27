40a3ecfe3093e8fbe77db1850138492a7f959a65
Shohan <shohanulislam19892@gmail.com>
2026-06-27 17:44:39 +0600

    Add Channels settings, ALLOWED_HOSTS, and Unfold backend configurations

--- core/settings.py
+++ core/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.UpdateLastActiveMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
