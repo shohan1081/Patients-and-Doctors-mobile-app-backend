2373399b679e5f59e0ba778508682573b74f605e
Shohan <shohanulislam19892@gmail.com>
2026-06-28 18:11:07 +0600

    Register video-consults route in appointments URLs

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     DoctorAvailabilityViewSet, AppointmentViewSet, GetAvailableSlotsView, 
-    DoctorPatientRelationViewSet, ProtocolViewSet, RecipeViewSet
+    DoctorPatientRelationViewSet, ProtocolViewSet, RecipeViewSet, VideoConsultViewSet
 )
 
 router = DefaultRouter()
@@ -11,6 +11,7 @@ router.register('bookings', AppointmentViewSet, basename='appointment')
 router.register('patients', DoctorPatientRelationViewSet, basename='doctor_patient_relation')
 router.register('protocols', ProtocolViewSet, basename='protocol')
 router.register('recipes', RecipeViewSet, basename='recipe')
+router.register('video-consults', VideoConsultViewSet, basename='video_consult')
 
 urlpatterns = [
     path('available-slots/', GetAvailableSlotsView.as_view(), name='available_slots'),
