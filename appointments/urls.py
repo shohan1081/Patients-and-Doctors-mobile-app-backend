209edd652f361dd0da5245d61a708ed606e543ee
Shohan <shohanulislam19892@gmail.com>
2026-06-28 16:26:15 +0600

    Register recipes routing in appointments app urls

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     DoctorAvailabilityViewSet, AppointmentViewSet, GetAvailableSlotsView, 
-    DoctorPatientRelationViewSet, ProtocolViewSet
+    DoctorPatientRelationViewSet, ProtocolViewSet, RecipeViewSet
 )
 
 router = DefaultRouter()
@@ -10,6 +10,7 @@ router.register('availability', DoctorAvailabilityViewSet, basename='doctor_avai
 router.register('bookings', AppointmentViewSet, basename='appointment')
 router.register('patients', DoctorPatientRelationViewSet, basename='doctor_patient_relation')
 router.register('protocols', ProtocolViewSet, basename='protocol')
+router.register('recipes', RecipeViewSet, basename='recipe')
 
 urlpatterns = [
     path('available-slots/', GetAvailableSlotsView.as_view(), name='available_slots'),
